9db7607b8b690830728880c169d0dda36fca0995
Franco Simurro <francosimurro7@gmail.com>
2021-12-11 15:35:48 -0300

    Initial Push

--- franco/lib.py
+++ franco/lib.py
@@ -1,8 +1,8 @@
-def try_me(palabra1, palabra2):
+def try_me(texto1, texto2):
 
     # Convertimos ambas a minúsculas
-    palabra1 = palabra1.lower()
-    palabra2 = palabra2.lower()
+    palabra1 = texto1.lower()
+    palabra2 = texto2.lower()
     # Convertimos las cadenas a lista.
     palabra1 = list(palabra1)
     palabra2 = list(palabra2)
@@ -13,16 +13,9 @@ def try_me(palabra1, palabra2):
     palabra1 = "".join(palabra1)
     palabra2 = "".join(palabra2)
     # Y finalmente comprobar si son iguales
-    return palabra1 == palabra2
+    return texto1 == texto2
 
-# Solicitar al usuario las cadenas
-texto1 = input("Ingresa un texto: ")
-texto2 = input("Ingresa otra texto: ")
-es_anagrama = try_me(texto1, texto2)
-if es_anagrama:
-    print(f"{texto1} es anagrama de {texto2}")
-else:
-    print(f"{texto1} NO es anagrama de {texto2}")
+
 
 
 if __name__ == "__main__":
